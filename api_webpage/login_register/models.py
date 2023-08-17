76d0a3f24107be59152db967932e6091e0eb64e1
LucasCto <1lucascastillox123@gmail.com>
2023-08-16 22:06:30 -0300

    Add: urls, templates and some code lines

--- api_webpage/login_register/models.py
+++ api_webpage/login_register/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.crypto import get_random_string
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    # Definición del modelo CustomUser
+    
+    access_token = models.CharField(max_length=100, blank=True, null=True)
+    
+    # Agregar el argumento related_name para evitar el conflicto en los accesos inversos
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customuser_set',
+        blank=True,
+        verbose_name='groups',
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_set',
+        blank=True,
+        verbose_name='user permissions',
+        help_text='Specific permissions for this user.',
+    )
+
+    def __str__(self):
+        return self.name
